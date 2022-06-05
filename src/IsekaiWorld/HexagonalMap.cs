19723a629aa8274285ff7a66e5bb63484262560d
Radek Falhar <Radek.Falhar@hotmail.com>
2022-06-05 18:49:23 +0200

    Add grass and dirt textures

--- src/IsekaiWorld/HexagonalMap.cs
+++ src/IsekaiWorld/HexagonalMap.cs
@@ -5,13 +5,24 @@ using Godot;
 public class HexagonalMap : Node2D
 {
 	private ArrayMesh _hexesMesh;
+	private ArrayMesh _grassSurfaceMesh;
+	private ArrayMesh _dirtSurfaceMesh;
+	
 	private HexagonNode _mouseoverHexagon;
 	private GameEntity _game;
 	
+	private Texture _grassTexture;
+	private Texture _dirtTexture;
+
 	public override void _Ready()
 	{
+		_grassTexture = ResourceLoader.Load<Texture>("res://Textures/Surface/grass.png");
+		_dirtTexture = ResourceLoader.Load<Texture>("res://Textures/Surface/dirt.jpg");
+		
 		_hexesMesh = new ArrayMesh();
-
+		_grassSurfaceMesh = new ArrayMesh();
+		_dirtSurfaceMesh = new ArrayMesh();
+		
 		_mouseoverHexagon = new HexagonNode
 		{
 			Color = Colors.Red
@@ -23,24 +34,53 @@ public class HexagonalMap : Node2D
 	{
 		var gameNode = GetNode<GameNode>("/root/GameNode");
 		_game = gameNode.GameEntity;
-		
+
 		base._EnterTree();
 	}
 
 	public void RefreshGameMap()
 	{
+		_grassSurfaceMesh.ClearSurfaces();
+		_dirtSurfaceMesh.ClearSurfaces();
 		_hexesMesh.ClearSurfaces();
 		foreach (var cell in _game.GameMap.Cells)
 		{
-			var center = cell.Position.Center(1);
-			Vector2[] points = new Vector2[7];
-			points[0] = center;
+			float textureScale;
+			
+			if (cell.Surface == SurfaceDefinitions.Grass)
+			{
+				textureScale = 0.2f;
+			}
+			else if (cell.Surface == SurfaceDefinitions.Dirt)
+			{
+				textureScale = 1.0f;
+			}
+			else
+			{
+				textureScale = 1;
+			}
+			
+			Vector2[] vertices = new Vector2[7];
+			Vector2[] textureUv = new Vector2[7];
+			vertices[0] = cell.Position.Center(1);
+			textureUv[0] = cell.Position.Center(textureScale);
 			for (int i = 0; i < 6; i++)
 			{
-				points[i + 1] = HexCubeCoord.HexCorner(cell.Position, 1, i);
+				vertices[i + 1] = HexCubeCoord.HexCorner(cell.Position, 1, i);
+				textureUv[i + 1] = HexCubeCoord.HexCorner(cell.Position, textureScale, i);
 			}
 
-			var hexColor = cell.Surface.Color;
+			Color hexColor;
+
+			if (cell.Surface == SurfaceDefinitions.Grass || cell.Surface == SurfaceDefinitions.Dirt)
+			{
+				hexColor = Colors.White;
+			}
+			else
+			{
+				hexColor = cell.Surface.Color;
+			}
+				
 
 			Color[] colors =
 			{
@@ -58,16 +98,31 @@ public class HexagonalMap : Node2D
 			};
 			var arrays = new Godot.Collections.Array();
 			arrays.Resize((int)ArrayMesh.ArrayType.Max);
-			arrays[(int)ArrayMesh.ArrayType.Vertex] = points;
+			arrays[(int)ArrayMesh.ArrayType.Vertex] = vertices;
+			arrays[(int)ArrayMesh.ArrayType.TexUv] = textureUv;
 			arrays[(int)ArrayMesh.ArrayType.Index] = indices;
 			arrays[(int)ArrayMesh.ArrayType.Color] = colors;
-			_hexesMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.TriangleFan, arrays);
+
+			if (cell.Surface == SurfaceDefinitions.Grass)
+			{
+				_grassSurfaceMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.TriangleFan, arrays);
+			}
+			else if (cell.Surface == SurfaceDefinitions.Dirt)
+			{
+				_dirtSurfaceMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.TriangleFan, arrays);				
+			}
+			else
+			{
+				_hexesMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.TriangleFan, arrays);                
+			}
 		}
 	}
 
 	public override void _Draw()
 	{
 		DrawMesh(_hexesMesh, null);
+		DrawMesh(_grassSurfaceMesh, _grassTexture);
+		DrawMesh(_dirtSurfaceMesh, _dirtTexture);
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
