d3810379cfcba4aacb6f931aa2f43d7bd9809ca4
Radek Falhar <Radek.Falhar@hotmail.com>
2022-06-07 18:57:08 +0200

    Rewrite how mesh is generated for map.
    Should both allow wall texturing and is more optimized.

--- src/IsekaiWorld/HexagonalMap.cs
+++ src/IsekaiWorld/HexagonalMap.cs
@@ -1,151 +1,169 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 public class HexagonalMap : Node2D
 {
-	private ArrayMesh _hexesMesh;
-	private ArrayMesh _grassSurfaceMesh;
-	private ArrayMesh _dirtSurfaceMesh;
-	
-	private HexagonNode _mouseoverHexagon;
-	private GameEntity _game;
-	
-	private Texture _grassTexture;
-	private Texture _dirtTexture;
+    private readonly Dictionary<SurfaceDefinition, ArrayMesh> _surfaceMeshes =
+        new Dictionary<SurfaceDefinition, ArrayMesh>();
 
-	public override void _Ready()
-	{
-		_grassTexture = ResourceLoader.Load<Texture>("res://Textures/Surface/grass.png");
-		_dirtTexture = ResourceLoader.Load<Texture>("res://Textures/Surface/dirt.jpg");
-		
-		_hexesMesh = new ArrayMesh();
-		_grassSurfaceMesh = new ArrayMesh();
-		_dirtSurfaceMesh = new ArrayMesh();
-		
-		_mouseoverHexagon = new HexagonNode
-		{
-			Color = Colors.Red
-		};
-		AddChild(_mouseoverHexagon);
-	}
+    private HexagonNode _mouseoverHexagon;
+    private GameEntity _game;
 
-	public override void _EnterTree()
-	{
-		var gameNode = GetNode<GameNode>("/root/GameNode");
-		_game = gameNode.GameEntity;
+    private Texture _grassTexture;
+    private Texture _dirtTexture;
 
-		base._EnterTree();
-	}
+    public override void _Ready()
+    {
+        _grassTexture = ResourceLoader.Load<Texture>("res://Textures/Surface/grass.png");
+        _dirtTexture = ResourceLoader.Load<Texture>("res://Textures/Surface/dirt.jpg");
 
-	public void RefreshGameMap()
-	{
-		_grassSurfaceMesh.ClearSurfaces();
-		_dirtSurfaceMesh.ClearSurfaces();
-		_hexesMesh.ClearSurfaces();
-		foreach (var cell in _game.GameMap.Cells)
-		{
-			float textureScale;
-			
-			if (cell.Surface == SurfaceDefinitions.Grass)
-			{
-				textureScale = 0.2f;
-			}
-			else if (cell.Surface == SurfaceDefinitions.Dirt)
-			{
-				textureScale = 1.0f;
-			}
-			else
-			{
-				textureScale = 1;
-			}
-			
-			Vector2[] vertices = new Vector2[7];
-			Vector2[] textureUv = new Vector2[7];
-			vertices[0] = cell.Position.Center(1);
-			textureUv[0] = cell.Position.Center(textureScale);
-			for (int i = 0; i < 6; i++)
-			{
-				vertices[i + 1] = HexCubeCoord.HexCorner(cell.Position, 1, i);
-				textureUv[i + 1] = HexCubeCoord.HexCorner(cell.Position, textureScale, i);
-			}
+        _mouseoverHexagon = new HexagonNode
+        {
+            Color = Colors.Red
+        };
+        AddChild(_mouseoverHexagon);
+    }
 
-			Color hexColor;
+    public override void _EnterTree()
+    {
+        var gameNode = GetNode<GameNode>("/root/GameNode");
+        _game = gameNode.GameEntity;
 
-			if (cell.Surface == SurfaceDefinitions.Grass || cell.Surface == SurfaceDefinitions.Dirt)
-			{
-				hexColor = Colors.White;
-			}
-			else
-			{
-				hexColor = cell.Surface.Color;
-			}
-				
+        base._EnterTree();
+    }
 
-			Color[] colors =
-			{
-				hexColor,
-				hexColor,
-				hexColor,
-				hexColor,
-				hexColor,
-				hexColor,
-				hexColor,
-			};
-			var indices = new[]
-			{
-				0, 1, 2, 3, 4, 5, 6, 1
-			};
-			var arrays = new Godot.Collections.Array();
-			arrays.Resize((int)ArrayMesh.ArrayType.Max);
-			arrays[(int)ArrayMesh.ArrayType.Vertex] = vertices;
-			arrays[(int)ArrayMesh.ArrayType.TexUv] = textureUv;
-			arrays[(int)ArrayMesh.ArrayType.Index] = indices;
-			arrays[(int)ArrayMesh.ArrayType.Color] = colors;
+    public void RefreshGameMap()
+    {
+        var surfaces = _game.GameMap.Cells.GroupBy(x => x.Surface);
 
-			if (cell.Surface == SurfaceDefinitions.Grass)
-			{
-				_grassSurfaceMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.TriangleFan, arrays);
-			}
-			else if (cell.Surface == SurfaceDefinitions.Dirt)
-			{
-				_dirtSurfaceMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.TriangleFan, arrays);				
-			}
-			else
-			{
-				_hexesMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.TriangleFan, arrays);                
-			}
-		}
-	}
+        foreach (var surfaceGroup in surfaces)
+        {
+            var surface = surfaceGroup.Key;
+            var cells = surfaceGroup.ToList();
 
-	public override void _Draw()
-	{
-		DrawMesh(_hexesMesh, null);
-		DrawMesh(_grassSurfaceMesh, _grassTexture);
-		DrawMesh(_dirtSurfaceMesh, _dirtTexture);
-	}
+            if (!_surfaceMeshes.TryGetValue(surface, out var mesh))
+            {
+                mesh = new ArrayMesh();
+                _surfaceMeshes[surface] = mesh;
+            }
+            else
+            {
+                mesh.ClearSurfaces();
+            }
 
-	public override void _UnhandledInput(InputEvent @event)
-	{
-		if (@event is InputEventMouseButton mouseButton)
-		{
-			if (mouseButton.ButtonIndex == (int)ButtonList.Left && mouseButton.Pressed)
-			{
-				var clickPosition = _mouseoverHexagon.HexPosition;
-				_game.UserInterface.MouseClickOnMap(clickPosition);
-			}
-		}
+            Color hexColor;
+            if (surface == SurfaceDefinitions.Grass || surface == SurfaceDefinitions.Dirt)
+            {
+                hexColor = Colors.White;
+            }
+            else
+            {
+                hexColor = surface.Color;
+            }
 
-		base._Input(@event);
-	}
+            float textureScale;
+            if (surface == SurfaceDefinitions.Grass)
+            {
+                textureScale = 0.2f;
+            }
+            else if (surface == SurfaceDefinitions.Dirt)
+            {
+                textureScale = 1.0f;
+            }
+            else
+            {
+                textureScale = 1;
+            }
 
-	public override void _Process(float delta)
-	{
-		var position = GetLocalMousePosition();
+// ReSharper disable InconsistentNaming
+            var verticesCount = 3 * 6 * cells.Count;
+            Vector2[] vertices_ = new Vector2[verticesCount];
+            Vector2[] textureUv = new Vector2[verticesCount];
+            Color[] colors___ = new Color[verticesCount];
+// ReSharper restore InconsistentNaming
 
-		var hex = HexCubeCoord.FromPosition(position, 1);
-		_mouseoverHexagon.HexPosition = hex;
+            for (int cellIndex = 0; cellIndex < cells.Count; cellIndex++)
+            {
+                var cell = cells[cellIndex];
 
-		base._Process(delta);
-	}
+                var verticleCenter = cell.Position.Center(1);
+                var textureCenter = cell.Position.Center(textureScale);
+
+                for (int i = 0; i < 6; i++)
+                {
+                    var triangleIndex = cellIndex * 6 + i;
+                    var hexCornerA = i;
+                    var hexCornerB = (i + 1) % 6;
+                    
+                    vertices_[triangleIndex * 3 + 0] = verticleCenter;
+                    textureUv[triangleIndex * 3 + 0] = textureCenter;
+                    colors___[triangleIndex * 3 + 0] = hexColor;
+                    vertices_[triangleIndex * 3 + 1] = HexCubeCoord.HexCorner(cell.Position, 1, hexCornerA);
+                    textureUv[triangleIndex * 3 + 1] = HexCubeCoord.HexCorner(cell.Position, textureScale, hexCornerA);
+                    colors___[triangleIndex * 3 + 1] = hexColor;
+                    vertices_[triangleIndex * 3 + 2] = HexCubeCoord.HexCorner(cell.Position, 1, hexCornerB);
+                    textureUv[triangleIndex * 3 + 2] = HexCubeCoord.HexCorner(cell.Position, textureScale, hexCornerB);
+                    colors___[triangleIndex * 3 + 2] = hexColor;
+                }
+            }
+
+            var arrays = new Godot.Collections.Array();
+            arrays.Resize((int)ArrayMesh.ArrayType.Max);
+            arrays[(int)ArrayMesh.ArrayType.Vertex] = vertices_;
+            arrays[(int)ArrayMesh.ArrayType.TexUv] = textureUv;
+            arrays[(int)ArrayMesh.ArrayType.Color] = colors___;
+
+            mesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, arrays);
+        }
+
+        Update();
+    }
+
+    public override void _Draw()
+    {
+        foreach (var pair in _surfaceMeshes)
+        {
+            var surface = pair.Key;
+            var mesh = pair.Value;
+
+            Texture texture = null;
+
+            if (surface == SurfaceDefinitions.Grass)
+            {
+                texture = _grassTexture;
+            }
+            else if (surface == SurfaceDefinitions.Dirt)
+            {
+                texture = _dirtTexture;
+            }
+
+            DrawMesh(mesh, texture);
+        }
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseButton)
+        {
+            if (mouseButton.ButtonIndex == (int)ButtonList.Left && mouseButton.Pressed)
+            {
+                var clickPosition = _mouseoverHexagon.HexPosition;
+                _game.UserInterface.MouseClickOnMap(clickPosition);
+            }
+        }
+
+        base._Input(@event);
+    }
+
+    public override void _Process(float delta)
+    {
+        var position = GetLocalMousePosition();
+
+        var hex = HexCubeCoord.FromPosition(position, 1);
+        _mouseoverHexagon.HexPosition = hex;
+
+        base._Process(delta);
+    }
 }
