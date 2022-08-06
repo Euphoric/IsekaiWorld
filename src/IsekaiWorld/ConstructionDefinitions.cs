1d6586ad2862b69cc4c1a278ac6aa32b36f03172
Radek Falhar <Radek.Falhar@hotmail.com>
2022-08-06 10:06:14 +0200

    Bed and chair require wood to construct.

--- src/IsekaiWorld/ConstructionDefinitions.cs
+++ src/IsekaiWorld/ConstructionDefinitions.cs
@@ -12,8 +12,8 @@ public static class ConstructionDefinitions
     
     public static readonly ConstructionDefinition TileFloor = new ConstructionDefinition("Core.Floor.Tile", "Tile Floor", null, SurfaceDefinitions.TileFloor.Id, 60);
     
-    public static readonly ConstructionDefinition WoodenChair = new ConstructionDefinition("Core.Furniture.Chair.Wood", "Wooden Chair", BuildingDefinitions.WoodenChair.Id, null, 120);
-    public static readonly ConstructionDefinition WoodenBed = new ConstructionDefinition("Core.Furniture.Bed.Wood", "Wooden Bed", BuildingDefinitions.WoodenBed.Id, null, 120);
+    public static readonly ConstructionDefinition WoodenChair = new ConstructionDefinition("Core.Furniture.Chair.Wood", "Wooden Chair", BuildingDefinitions.WoodenChair.Id, null, 120, ItemDefinitions.Wood);
+    public static readonly ConstructionDefinition WoodenBed = new ConstructionDefinition("Core.Furniture.Bed.Wood", "Wooden Bed", BuildingDefinitions.WoodenBed.Id, null, 120, ItemDefinitions.Wood);
     public static readonly ConstructionDefinition TableStoveFueled = new ConstructionDefinition("Core.Production.StoveFueled", "Fueled stove", BuildingDefinitions.TableStoveFueled.Id, null, 120);
     
     private static readonly Dictionary<string, ConstructionDefinition> DefinitionsMap =
