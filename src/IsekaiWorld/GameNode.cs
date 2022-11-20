1d65c53a62bea1e9b146699b423d88aa548a096e
Radek Falhar <Radek.Falhar@hotmail.com>
2022-11-20 09:41:38 +0100

    Implement game loop thread separate from rendering thread.

--- src/IsekaiWorld/GameNode.cs
+++ src/IsekaiWorld/GameNode.cs
@@ -8,19 +8,23 @@ public partial class GameNode : Node
 	private MapItemView _mapItemView = null!;
 	private CharacterView _characterView = null!;
 	private UserInterface _userInterface = null!;
+	private System.Threading.Thread _gameThread = null!;
+
 	
 	private readonly MessagingHub _viewMessagingHub = new MessagingHub();
 	
 	[Obsolete("Should not be accessible to view layer")]
 	public GameEntity GameEntity => _game;
-	public HexagonalMap MapNode { get; private set; }
+
+	public HexagonalMap MapNode { get; private set; } = null!;
 
 	public override void _EnterTree()
 	{
 		_game = new GameEntity();
 		_game.Messaging.ConnectMessageHub(_viewMessagingHub);
-		
-		
+
+		_gameThread = new System.Threading.Thread(GameLoop);
+
 		_characterView = new CharacterView(this);
 		_viewMessagingHub.Register(_characterView.Messaging);
 		_buildingView = new BuildingView(this);
@@ -32,11 +36,16 @@ public partial class GameNode : Node
 		_viewMessagingHub.Register(_userInterface.Messaging);
 		MapNode = GetNode<HexagonalMap>("Map/HexagonalMap");
 		_viewMessagingHub.Register(MapNode.Messaging);
-		
+
 		base._EnterTree();
 	}
 
 	public override void _Ready()
+	{
+		_gameThread.Start();
+	}
+
+	private void GameLoop()
 	{
 		var mapGenerator = new MapGenerator();
 		//var mapGenerator = new WallTilingTestMapGenerator();
@@ -47,12 +56,18 @@ public partial class GameNode : Node
 		adamCharacter.Position = new HexCubeCoord(1, 1, -2);
 		// var eveCharacter = _game.AddCharacter("Eve");
 		// eveCharacter.Position = new HexCubeCoord(1, -1, 0);
-	}
 
+		while (true)
+		{
+			_game.Update();
+			
+			// TODO: Sleep/Spinwait dynamically to achieve consistent TPS
+			System.Threading.Thread.Sleep(13);
+		}
+	}
+	
 	public override void _Process(double delta)
 	{
-		_game.Update();
-		
 		_viewMessagingHub.DistributeMessages();
 		
 		_characterView.Update();
